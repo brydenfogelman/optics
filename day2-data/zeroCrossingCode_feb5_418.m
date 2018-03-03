716b3f7c40a888d22ded1ff29439ade12c50bd6f
Bryden Fogelman <bfogelman@tau.phas.ubc.ca>
2018-03-03 15:29:10 -0800

    saving data to git so I can re-access it if I blow away data by accident

---
+++ day2-data/zeroCrossingCode_feb5_418.m
@@ -0,0 +1,11 @@
+
+%find zero crossings
+zc = data > mean(data) & circshift(data,1)<mean(data) | data < mean(data)...
+    & circshift(data,1)>mean(data);
+ figure
+ plot(data(1:50)/max(data),'kx-');
+ hold on;
+ plot(zc(1:50)*mean(data)/max(data),'o')
+ lambda = 632.8e-9;
+ D = 20e-6;
+ K = lambda*sum(zc)/2/2/D;
