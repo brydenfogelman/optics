09de4ab8ce679337146ff804c778e9c0a58ce06d
Bryden Fogelman <bfogelman@tesla.com>
2018-03-10 09:49:41 -0800

    day3 data

--- day2-data/zeroCrossingCode_feb5_418.m
+++ day2-data/zeroCrossingCode_feb5_418.m
@@ -7,5 +7,5 @@ zc = data > mean(data) & circshift(data,1)<mean(data) | data < mean(data)...
  hold on;
  plot(zc(1:50)*mean(data)/max(data),'o')
  lambda = 632.8e-9;
- D = 20e-6;
+ D = 0.2e-3;
  K = lambda*sum(zc)/2/2/D;
