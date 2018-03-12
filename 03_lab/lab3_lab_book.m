2aaf674b08db6abaea6fe4e6592ee1971afc2fca
CaptNapalm <james.wasteneys@gmail.com>
2018-03-12 15:08:43 -0700

    Begun Lab book

---
+++ 03_lab/lab3_lab_book.m
@@ -0,0 +1,17 @@
+%% OPTICAL CAVITY - PHYS 408 L2D
+%
+%
+%  James Wasteneys - 34140137
+%  Bryden Fogelman - 36294130
+
+
+
+%% Mar 12 2018
+% 
+%  Day 1: Begin by aligning the experimental setup
+%  Start by removeing the close mirror walking the beam so that it aligns 
+%  with the far mirror at both close and far positions. Next put the lens in
+%  first aligning it vertically and then horizontally, with the reflection
+%  back towards the laser
+% 
+% 
