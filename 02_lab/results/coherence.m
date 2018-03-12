1798db869af697abd2c712b1a275d37f1c97cc60
Bryden Fogelman <bfogelman@tesla.com>
2018-03-12 11:52:36 -0700

    big directory changes

--- 02_lab/results/coherence.m
+++ 02_lab/results/coherence.m
@@ -1,4 +1,8 @@
-load('day3-data\zpl_span_3_speed_0_003_mar_5_414.mat')
+S = load('day3-data/zpl_span_3_speed_0_003_mar_5_414.mat');
+data = S.data2;
+
+K = 0.95;
+span = 3;
 
 data = data - mean(data); 
 data = data/max(abs(data));
@@ -10,11 +14,16 @@ fftdata = abs(fftshift(fft(data,NFFT)));
 fftdata_oneside = fftdata(NFFT/2+2:NFFT);
 fftdata_oneside = fftdata_oneside/max(fftdata_oneside);
 
-plot(data)
+% plot(data)
 [upr_peakEnv,lwr_peakEnv] = envelope(data,200,'peak');
-figure
-plot(lwr_peakEnv)
-envelope(lwr_peakEnv,200,'peak')
+% figure
+% plot(lwr_peakEnv)
+% envelope(lwr_peakEnv,200,'peak')
 [amp,idx] = findpeaks(-lwr_peakEnv);
 
+x = idx / max(idx) * 2 * span * K;
+y = amp;
 
+xdata = linspace(0, 6*0.95, length(data));
+
+coherence_sodium_fit(x,y, xdata, data);
